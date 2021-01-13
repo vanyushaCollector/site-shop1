cbd62cc1c788e751a999ddfb63aea9e9fe1ebfa2
vanyushaCollector <77235952+vanyushaCollector@users.noreply.github.com>
2021-01-14 01:19:42 +0700

    added a list of popular products

--- js/main.js
+++ js/main.js
@@ -3,23 +3,32 @@ $(function(){
 		dots: true,
 		prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arow-left.svg"></button>',
 		nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg"></button>',
-		
+
 	});
 
 	$('.tab').on('click', function(e){
 		e.preventDefault();
 
-		$('.tab').removeClass('tab--active');
-		$('.tabs-content').removeClass('tabs-content--active');
-		
+		$($(this).siblings()).removeClass('tab--active');
+		$($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+
 		$(this).addClass('tab--active')
 		$($(this).attr('href')).addClass('tabs-content--active')
 
 	});
 
 	$('.product-item__favorite').on('click', function(){
-		$('.product-item__favorite').toggleClass('product-item__favorite--active')
+		$(this).toggleClass('product-item__favorite--active')
 	})
 
-});
+	$('.product-slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 3,
+		prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arow-black-left.svg"></button>',
+		nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg"></button>',
 
+
+	})
+
+
+});
